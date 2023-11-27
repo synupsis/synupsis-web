dc18208b1efcd065ddf5a10c1d41ef61e68bbbd4
valentingbt <valentinguibert@hotmail.fr>
2023-11-27 19:31:11 +0100

    feat(all): init tailwind and logo

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="w-full h-screen items-center flex justify-center">
+      <img class="h-56 w-56" src="~/assets/svg/logo.svg"  alt="Synupsis"/>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
