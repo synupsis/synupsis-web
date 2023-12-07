075c3496c016903a6b118f3e5c845396f9310dbd
valentingbt <valentinguibert@hotmail.fr>
2023-12-07 21:36:16 +0100

    feat(all): fix flickering tagline

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,17 @@
       <img src="~/assets/svg/logo.svg" alt="Synupsis"/>
       <div class="bg"></div>
     </div>
-    <p class="italic">{{ randomTagline }}</p>
+    <div class="h-6">
+      <TransitionRoot :show="!!randomTagline"
+                      enter="transition-opacity duration-75"
+                      enter-from="opacity-0"
+                      enter-to="opacity-100"
+                      leave="transition-opacity duration-150"
+                      leave-from="opacity-100"
+                      leave-to="opacity-0">
+        <p class="italic">{{ randomTagline }}</p>
+      </TransitionRoot>
+    </div>
     <div class="flex items-center justify-center gap-4 w-full px-4">
       <Combobox :fetch-items="searchShows" :items="items" :items-loading="loading" :click-action="goToShow"
                 class="w-full sm:w-80" :show-chevron-icon="false"
@@ -20,6 +30,7 @@
 import Combobox from "~/components/ui/Combobox.vue";
 import useSupabase from "~/composables/useSupabase";
 import Alert from "~/components/ui/Alert.vue";
+import {TransitionRoot} from '@headlessui/vue'
 
 const router = useRouter()
 
@@ -28,8 +39,10 @@ const {supabase} = useSupabase();
 const items = ref([]);
 const loading = ref(false);
 const error = ref(null);
+const randomTagline = ref('');
 
-const taglines = [
+onMounted(() => {
+  const taglines = [
     "Short memory, but don't worry.",
     "Quick Summaries for Every Show",
     "Never Forget What Happened",
@@ -39,9 +52,10 @@ const taglines = [
     "Every Series Summarized.",
     "Your Shortcut to Storylines.",
     "Summing Up Stories"
-]
+  ]
 
-const randomTagline = taglines[Math.floor(Math.random() * taglines.length)];
+  randomTagline.value = taglines[Math.floor(Math.random() * taglines.length)];
+})
 
 const searchShows = async (value: any) => {
   loading.value = true;
