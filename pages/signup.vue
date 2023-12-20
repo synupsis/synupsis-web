d02a33d52d3d38b91e4519727e8e3fc0e4e33b7b
FrogSkyWater <mael.billieux@hotmail.fr>
2023-12-20 15:25:59 +0100

    add login and signup front page

---
+++ pages/signup.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-black py-6 sm:py-12">
+        <div
+            class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 m-8">
+
+            <div class="flex justify-between gap-4 items-center mb-10">
+                <router-link to="/">
+                    <Logo class="h-16 w-16" />
+                </router-link>
+                <div class="text-black text-2xl font-black inline-block">Signup</div>
+            </div>
+
+            <div class="mb-6">
+                <div class="text-black font-semibold">Username</div>
+                <label class="relative block">
+                    <span class="sr-only">username</span>
+                    <input
+                        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-xs"
+                        placeholder="Enter your username" type="text" style="color:black" name="usernameinput" />
+                </label>
+            </div>
+
+            <div class="mb-2">
+                <div class="text-black font-semibold">Email</div>
+                <label class="relative block">
+                    <span class="sr-only">Mailinput</span>
+                    <input
+                        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-xs"
+                        placeholder="Enter your email here" type="email" style="color:black" name="emailinput" />
+                </label>
+            </div>
+
+            <div class="mb-6">
+                <div class="text-black font-semibold">Confirm Email</div>
+                <label class="relative block">
+                    <span class="sr-only">Mailinput</span>
+                    <input
+                        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-xs"
+                        placeholder="Confirm your email" type="email" style="color:black" name="emailconfirmationinput" />
+                </label>
+            </div>
+
+            <div class="mb-2">
+                <div class="text-black font-semibold">Password</div>
+                <label class="relative block">
+                    <span class="sr-only">Pwdinput</span>
+                    <input
+                        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-xs"
+                        placeholder="Enter your password here" type="password" style="color:black" name="pwdinput" />
+                </label>
+            </div>
+
+            <div class="flex grow justify-around gap-1">
+                <div class="pl-2">
+                    <div class="border border-slate-300 bg-slate-200 h-1 w-16 rounded-lg"></div>
+                </div>
+                <div class="grow border border-slate-300 bg-slate-200 h-1 w-16 rounded-lg"></div>
+                <div class="grow border border-slate-300 bg-slate-200 h-1 w-16 rounded-lg"></div>
+                <div class="grow border border-slate-300 bg-slate-200 h-1 w-16 rounded-lg"></div>
+                <div class="grow border border-slate-300 bg-slate-200 h-1 w-16 rounded-lg"></div>>
+            </div>
+
+            <div class="mb-2">
+                <div class="text-black font-semibold">Confirm password</div>
+                <label class="relative block">
+                    <span class="sr-only">Pwdinput</span>
+                    <input
+                        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 mb-12 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-xs"
+                        placeholder="Confirm your password" type="password" style="color:black" name="confirmpwdinput" />
+                </label>
+            </div>
+
+            
+
+
+            <div class="flex justify-center mb-1">
+                <button class="w-40 h-10 rounded-lg text-white bg-purple-950 hover:bg-purple-500 transition">Register</button>
+            </div>
+
+            <div class="flex justify-center gap-1">
+                <p class="text-black text-sm">Already registered ?</p>
+                <div class="text-black text-sm underline underline-offset-1">
+                    <a href="http://localhost:3000/login">Login</a> <!-- remplacer par le lien définitif-->
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</template>
