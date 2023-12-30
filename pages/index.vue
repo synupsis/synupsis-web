ba6442a3b7f35d747fff336d85a8211b73645174
valentingbt <valentinguibert@hotmail.fr>
2023-12-30 20:26:36 +0100

    feat(all): fix merge

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,44 @@
 <template>
-  <div class='w-full h-screen flex flex-col items-center gap-4 sm:justify-center'>
-    <div class="text-right w-screen pt-6 pr-4">
-      <div v-if="user">{{ user.email }}</div>
-      <div v-else class="flex gap-4 items-center justify-end">
-        <router-link to="/login"><Button>Log in</Button></router-link>
-        <router-link to="/signup"><Button>Sign up</Button></router-link>
+  <div>
+    <div class='text-right w-full pt-6 pr-4'>
+      <div v-if='user'>{{ user.email }}</div>
+      <div v-else class='flex gap-4 items-center justify-end'>
+        <router-link to='/login'>
+          <Button>Log in</Button>
+        </router-link>
+        <router-link to='/signup'>
+          <Button>Sign up</Button>
+        </router-link>
       </div>
     </div>
-    <Logo class='h-56 w-56' />
-    <div class='h-6'>
-      <TransitionRoot :show='!!randomTagline'
-                      enter='transition-opacity duration-150'
-                      enter-from='opacity-0'
-                      enter-to='opacity-100'
-                      leave='transition-opacity duration-150'
-                      leave-from='opacity-100'
-                      leave-to='opacity-0'>
-        <p class='italic'>{{ randomTagline }}</p>
-      </TransitionRoot>
-    </div>
-    <div class='flex items-center justify-center gap-4 w-full px-4'>
-      <Combobox :click-action='goToShow' :fetch-items='searchShows' :items='items' :items-loading='loading'
-                :show-chevron-icon='false' class='w-full sm:w-80'
-                placeholder='Search for a show'></Combobox>
-    </div>
-    <Alert v-if='error' title='Something went wrong'>
-      <p>There was an error</p>
-      <p>{{ error }}</p>
+    <div class='w-full h-screen flex flex-col items-center gap-4 sm:justify-center'>
+      <Logo class='h-56 w-56' />
+      <div class='h-6'>
+        <TransitionRoot :show='!!randomTagline'
+                        enter='transition-opacity duration-150'
+                        enter-from='opacity-0'
+                        enter-to='opacity-100'
+                        leave='transition-opacity duration-150'
+                        leave-from='opacity-100'
+                        leave-to='opacity-0'>
+          <p class='italic'>{{ randomTagline }}</p>
+        </TransitionRoot>
+      </div>
+      <div class='flex items-center justify-center gap-4 w-full px-4'>
+        <Combobox :click-action='goToShow' :fetch-items='searchShows' :items='items' :items-loading='loading'
+                  :show-chevron-icon='false' class='w-full sm:w-80'
+                  placeholder='Search for a show'></Combobox>
+      </div>
+      <Alert v-if='error' title='Something went wrong'>
+        <p>There was an error</p>
+        <p>{{ error }}</p>
 
-    </Alert>
+      </Alert>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang='ts' setup>
 import Combobox from '~/components/ui/Combobox.vue';
 import useSupabase from '~/composables/useSupabase';
 import Alert from '~/components/ui/Alert.vue';
@@ -48,7 +54,7 @@ const randomTagline = ref('');
 
 onMounted(() => {
   const taglines = [
-    "Short memory, but don't worry.",
+    'Short memory, but don\'t worry.',
     'Quick Summaries for Every Show',
     'Never Forget What Happened',
     'Binge the Summary, Savor the Show.',
