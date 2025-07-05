992966aced8426b0ca95f4d5446fbeae5d461200
valentinguibert <valentinguibert@hotmail.fr>
2025-07-05 12:59:41 +0200

    feat(recap-editor): update RecapCanvas key binding for improved rendering

--- pages/recap-editor.vue
+++ pages/recap-editor.vue
@@ -111,7 +111,7 @@
             v-else
             class="relative aspect-[9/19.5] h-full max-w-full bg-background rounded-3xl shadow-lg"
           >
-            <RecapCanvas v-model="activeSlideCanvas" :loading="loading" />
+            <RecapCanvas :key="selectedSlideId" v-model="activeSlideCanvas" :loading="loading" />
           </div>
         </main>
       </div>
