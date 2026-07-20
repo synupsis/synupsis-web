e2ec0cb281a386274c136b8cfdac749114e5fb22
Valentin Guibert <valentinguibert@hotmail.fr>
2026-07-20 10:48:03 +0200

    fix(supabase): support PostgreSQL 17 migrations

--- supabase/migrations/20231219145602_remote_schema.sql
+++ supabase/migrations/20231219145602_remote_schema.sql
@@ -10,8 +10,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
-
 ALTER SCHEMA "public" OWNER TO "postgres";
 
 CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
@@ -19,8 +17,6 @@ CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
 CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
-
-CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
 
 CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
 
